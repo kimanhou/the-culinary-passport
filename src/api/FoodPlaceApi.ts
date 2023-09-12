5968bdf8c43a6c3725c8b376ac65b26820ffc58f
Kim Anh Nguyen <kimanhou@gmail.com>
2023-09-12 10:02:38 +0900

    🚑️ Fix path

--- src/api/FoodPlaceApi.ts
+++ src/api/FoodPlaceApi.ts
@@ -2,7 +2,7 @@ import { arrayHandler } from "../model/deserialization/DeserializationUtils";
 import FoodPlace from "../model/FoodPlace";
 import { get } from "./JsonApiEndpoint";
 
-export const FOOD_PLACE_API_ENDPOINT = "/data.json";
+export const FOOD_PLACE_API_ENDPOINT = "data.json";
 
 const FoodPlaceApi = {
     get: get(FOOD_PLACE_API_ENDPOINT, arrayHandler(FoodPlace.deserialize)),
