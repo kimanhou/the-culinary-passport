425a952c311762651a3ec370034986f9b7322eea
Kim Anh Nguyen <kimanhou@gmail.com>
2023-09-22 11:35:34 +0900

    🎨 Move filter

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import { FoodPlaceController } from "../../api/FoodPlaceController";
 import CityModel from "../../model/City";
 import FoodPlace from "../../model/FoodPlace";
-import FilterDropDown from "../FilterDropDown/FilterDropDown";
-import FoodPlaceList from "../FoodPlaceList/FoodPlaceList";
+import FoodPlaceListWithFilter from "../FoodPlaceListWithFilter/FoodPlaceListWithFilter";
 import LoadData from "../LoadData";
 import "./City.scss";
 
@@ -14,53 +13,18 @@ interface ICityProps {
 
 const City: React.FC<ICityProps> = (props) => {
     const [promise, setPromise] = useState(new Promise<FoodPlace[]>(() => {}));
-    const [typeOfCuisineOptions, setTypeOfCuisineOptions] = useState<string[]>(
-        []
-    );
-    const [selectedCuisine, setSelectedCuisine] = useState<string>("");
 
     useEffect(() => {
         const controller = new FoodPlaceController(props.city);
-        const foodPlacesPromise = controller.get();
-        setPromise(() => foodPlacesPromise);
-        foodPlacesPromise.then((foodPlaces) => {
-            setTypeOfCuisineOptions(
-                Array.from(
-                    new Set<string>(foodPlaces.flatMap((v) => v.typeOfCuisine))
-                )
-            );
-
-            if (
-                selectedCuisine !== "" &&
-                selectedCuisine.toLocaleLowerCase() !== "all"
-            ) {
-                setPromise(
-                    () =>
-                        new Promise<FoodPlace[]>((resolve, reject) =>
-                            resolve(
-                                foodPlaces.filter((t) =>
-                                    t.typeOfCuisine.includes(selectedCuisine)
-                                )
-                            )
-                        )
-                );
-            }
-        });
-    }, [props.city, selectedCuisine]);
+        setPromise(() => controller.get());
+    }, [props.city]);
 
     return (
         <section id="city">
             <h1>{props.city.name.toLocaleUpperCase()}</h1>
-            {typeOfCuisineOptions.length > 0 && (
-                <FilterDropDown
-                    options={typeOfCuisineOptions}
-                    selectedOption={selectedCuisine}
-                    setSelectedOption={setSelectedCuisine}
-                />
-            )}
             <LoadData promise={promise}>
                 {(foodPlaceList) => (
-                    <FoodPlaceList foodPlaceList={foodPlaceList} />
+                    <FoodPlaceListWithFilter foodPlaces={foodPlaceList} />
                 )}
             </LoadData>
         </section>
