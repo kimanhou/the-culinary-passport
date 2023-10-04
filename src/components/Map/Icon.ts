b6010887b373b2bf3e4b0bc807c189b27ee69aa6
Kim Anh Nguyen <kimanhou@gmail.com>
2023-10-05 07:35:42 +0900

    Image carousel (#15)
    * 💄 Smaller Map icon
    
    * ✨ Image carousel
    
    * 📄 Add Spontini

--- src/components/Map/Icon.ts
+++ src/components/Map/Icon.ts
@@ -3,7 +3,7 @@ import L from "leaflet";
 const icon = new L.Icon({
     iconUrl: require("../FoodPlaceList/googleMaps.png"),
     iconRetinaUrl: require("../FoodPlaceList/googleMaps.png"),
-    iconSize: new L.Point(25, 41),
+    iconSize: new L.Point(18, 30),
     className: "leaflet-div-icon",
 });
 
