145277c7d8c9c4863f384fe64a8b6c431f55a6ad
Kim Anh Nguyen <kimanhou@gmail.com>
2024-02-11 17:55:38 +0900

    Data london (#22)
    * 📝 Add markets
    
    * 🌶️ Add Dishoom
    
    * 🍕 Add Pizza Union
    
    * 🍕 Add Pizza Pilgrims and Gloria
    
    * 🍔 Patty and Bun
    
    * 🍔 Honest Burgers
    
    * 🐟🍟 Poppies
    
    * 🐷 The Pig and the Butcher and Yauatcha
    
    * 🍺 Rosemary
    
    * 🍺 The Crown and the Shuttle
    
    * 🍺 Add pubs
    
    * 📅 Update date in footer
    
    * Update data_london.json (#21)
    
    ---------
    
    Co-authored-by: Mark Poussard <poussard.mark@gmail.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./Footer.scss";
 
 export const Footer: React.FC = (props) => {
-    return <footer className="flex-row">&copy; 2023</footer>;
+    return <footer className="flex-row">&copy; 2024</footer>;
 };
