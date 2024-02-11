696dad25a6e40091fd3f2a83d682a8701dedf1b5
Kim Anh Nguyen <kimanhou@gmail.com>
2024-02-11 18:04:03 +0900

    💄 Try new header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "components/Navigation/Navigation";
 import "./Header.scss";
-import logo from "./food-blog-logo.png";
-import City from "../../model/City";
+// import logo from "./food-blog-logo.png";
+import City from "model/City";
+import Divider from "components/common/Divider/Divider";
 
 interface IHeaderProps {
     cities: City[];
@@ -10,12 +11,21 @@ interface IHeaderProps {
 
 export const Header: React.FC<IHeaderProps> = (props) => {
     return (
-        <header className="flex-row">
-            <a href="./">
+        <header className="flex-column">
+            {/* <a href="./">
                 <img src={logo} className={`logo-image`} alt={"logo"} />
-            </a>
+            </a> */}
+            <div className="tag-line flex-row align-items-center">
+                <div className="tag-line-decoration left" />
+                <h4>
+                    <i>sibling edition</i>
+                </h4>
+                <div className="tag-line-decoration right" />
+            </div>
 
+            <h1 className="typeface-primary">Culinary passport</h1>
             <Navigation cities={props.cities} />
+            <Divider />
         </header>
     );
 };
---
+++ src/components/common/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Divider.scss";
+
+const Divider: React.FC = (props) => {
+    return <div className="divider"></div>;
+};
+
+export default Divider;
