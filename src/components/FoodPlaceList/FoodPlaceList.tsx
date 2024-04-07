36d3580104e22904b4b27aed0c3308aa60b3a923
Kim Anh Nguyen <kimanhou@gmail.com>
2024-04-07 19:47:04 +0900

    🐛 Fix alphabetical sorting

--- src/components/FoodPlaceList/FoodPlaceList.tsx
+++ src/components/FoodPlaceList/FoodPlaceList.tsx
@@ -13,25 +13,15 @@ interface IFoodPlaceListProps {
 const FoodPlaceList: React.FC<IFoodPlaceListProps> = (props) => {
     return (
         <ul className="food-place-list flex-row justify-content-center">
-            {props.foodPlaceList
-                .sort(function (a, b) {
-                    if (a.name < b.name) {
-                        return -1;
-                    }
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    return 0;
-                })
-                .map((t) => (
-                    <li key={t.name}>
-                        <FoodPlaceCardFullscreenWrapper
-                            city={props.city}
-                            foodPlace={t}
-                            onLike={props.onLike}
-                        />
-                    </li>
-                ))}
+            {props.foodPlaceList.map((t) => (
+                <li key={t.name}>
+                    <FoodPlaceCardFullscreenWrapper
+                        city={props.city}
+                        foodPlace={t}
+                        onLike={props.onLike}
+                    />
+                </li>
+            ))}
         </ul>
     );
 };
