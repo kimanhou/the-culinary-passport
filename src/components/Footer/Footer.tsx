e69001cbf366d85c1afcc6a8ac680e5f64d8f59e
Kim Anh Nguyen <kimanhou@gmail.com>
2025-01-06 17:34:00 +0100

    🎨 Update Bouillon Pigalle and year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.scss";
 
-export const Footer: React.FC = (props) => {
-    return <footer className="flex-row">&copy; 2024</footer>;
+export const Footer: React.FC = () => {
+    const currentYear = new Date().getFullYear();
+    return <footer className="flex-row">&copy; {currentYear}</footer>;
 };
