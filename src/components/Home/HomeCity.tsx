70a88b991d4514a9497cfc7ff4e3667a0e0210c0
Kim Anh Nguyen <kimanhou@gmail.com>
2025-02-25 18:50:33 +0100

    💄 Redesign Home page

--- src/components/Home/HomeCity.tsx
+++ src/components/Home/HomeCity.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { CityEnum } from "ts/enum";
+import City from "model/City";
+import "./HomeCity.scss";
 
 interface IHomeCityProps {
-    city: CityEnum;
+    city: City;
 }
 
 const HomeCity: React.FC<IHomeCityProps> = (props) => {
     return (
         <Link
-            className="home-city flex-row"
-            to={props.city.toLocaleLowerCase()}
+            className="home-city"
+            to={props.city.name.toLocaleLowerCase()}
+            style={{ backgroundImage: `url(${props.city.imageUrl})` }}
         >
-            {props.city}
+            {props.city.name}
         </Link>
     );
 };
